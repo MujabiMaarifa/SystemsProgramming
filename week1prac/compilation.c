a1e51279d07215e60f6873bd3e5ca9ab0df80961
MujabiMaarifa <daudimujabi@gmail.com>
2026-07-25 10:02:54 -0400

    Compilation error occurs due to syntax errors

---
+++ week1prac/compilation.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hellow world");
+        return 0;
+}
+
+/*How compilation occurs in C
+ *Preprocessing - handles the directives, macros and header files
+ *Compilation - changes the source code to assembly code
+ *Assembly - changes the assembly code into objects
+ *linking - links the object files and libraries into executable
+ * */
