daf74cd055374413d202250030f4c7ed8d02eb15
MujabiMaarifa <daudimujabi@gmail.com>
2026-07-30 18:09:04 -0400

    added threads in displaythread worker

--- threads/display_ptid.c
+++ threads/display_ptid.c
@@ -8,8 +8,21 @@ void *worker(void *arg)
 }
 int main()
 {
-    pthread_t t1;
+    printf("Creating thread ...\n");
+
+    pthread_t t1, t2, t3, t4, t5;
+
     pthread_create(&t1,NULL,worker,NULL);
+    pthread_create(&t2,NULL,worker,NULL);
+    pthread_create(&t3,NULL,worker,NULL);
+    pthread_create(&t4,NULL,worker,NULL);
+    pthread_create(&t5,NULL,worker,NULL);
+
     pthread_join(t1,NULL);
+    pthread_join(t2,NULL);
+    pthread_join(t3,NULL);
+    pthread_join(t4,NULL);
+    pthread_join(t5,NULL);
+
     return 0;
 }
